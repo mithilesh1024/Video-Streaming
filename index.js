2dbd96a13940ae7568e52718080b65d8bf03238c
Mithilesh Kapadi <mithileshkapadi08@gmail.com>
2021-01-04 17:58:19 +0530

    ejs

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000
+const dir = __dirname + '/video';
+const files = fs.readdirSync(dir);
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('home')
+    var name_files = [];
+    for(const file of files) {
+        name_files.push(file)
+    }
+    res.render('home', {names: name_files})
 });
 
 app.get('/playVideo', (req, res) => {
