6b44617bd9cb19d58aa6902975b468d9f9de6584
Mithilesh Kapadi <mithileshkapadi08@gmail.com>
2021-06-10 12:32:58 +0530

    flask init

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
-from pathlib import Path
-from fastapi import FastAPI
-from fastapi import Request, Response
-from fastapi import Header
-from fastapi.templating import Jinja2Templates
+from flask import Flask, render_template, redirect
 import os
 
-
-app = FastAPI()
-templates = Jinja2Templates(directory="template")
+app = Flask(__name__)
 CHUNK_SIZE = 1024*1024
-video_path = Path("video.mp4")
 path = "C:\\Users\\Admin\\Videos\\Captures"
 fileslist = []
+
 for root, dirs, files in os.walk(path):
 	for file in files:
-		fileslist.append(os.path.join(root,file))
-# print(fileslist)
+            if file[-4:] == ".mp4":
+                fileslist.append(os.path.join(root,file))
 
-# @app.get("/")
+@app.route("/")
+def home():
+    # return render_template("home.html", {"fileslist": fileslist})
+    return "hello world"
+
+# @app.post("/{name}")
+# def video(request: Request, name: str):
+#     print(name)
+#     pass
+
+# @app.get("/playvideo")
 # async def read_root(request: Request):
 #     return templates.TemplateResponse("index.htm", context={"request": request})
 
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("home.html", context={"request": request})
+# @app.get("/video")
+# async def video_endpoint(range: str = Header(None)):
+#     start, end = range.replace("bytes=", "").split("-")
+#     start = int(start)
+#     end = int(end) if end else start + CHUNK_SIZE
+#     with open(video_path, "rb") as video:
+#         video.seek(start)
+#         data = video.read(end - start)
+#         filesize = str(video_path.stat().st_size)
+#         headers = {
+#             'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
+#             'Accept-Ranges': 'bytes'
+#         }
+#         return Response(data, status_code=206, headers=headers, media_type="video/mp4")
 
-@app.get("/video")
-async def video_endpoint(range: str = Header(None)):
-    start, end = range.replace("bytes=", "").split("-")
-    start = int(start)
-    end = int(end) if end else start + CHUNK_SIZE
-    with open(video_path, "rb") as video:
-        video.seek(start)
-        data = video.read(end - start)
-        filesize = str(video_path.stat().st_size)
-        headers = {
-            'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
-            'Accept-Ranges': 'bytes'
-        }
-        return Response(data, status_code=206, headers=headers, media_type="video/mp4")
+
+if __name__ == '__main__':
+    app.run(debug=True)
