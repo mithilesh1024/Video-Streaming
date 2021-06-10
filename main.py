a8b5c8dc23d238fae359cda61e4e5579e7151831
Mithilesh Kapadi <mithileshkapadi08@gmail.com>
2021-06-10 12:47:06 +0530

    changes

--- main.py
+++ main.py
@@ -15,27 +15,27 @@ fileslist = []
 for root, dirs, files in os.walk(path):
 	for file in files:
 		fileslist.append(os.path.join(root,file))
-# print(fileslist)
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("home.html", context={"request": request, "fileslist": fileslist})
+
 
 # @app.get("/")
 # async def read_root(request: Request):
 #     return templates.TemplateResponse("index.htm", context={"request": request})
 
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("home.html", context={"request": request})
-
-@app.get("/video")
-async def video_endpoint(range: str = Header(None)):
-    start, end = range.replace("bytes=", "").split("-")
-    start = int(start)
-    end = int(end) if end else start + CHUNK_SIZE
-    with open(video_path, "rb") as video:
-        video.seek(start)
-        data = video.read(end - start)
-        filesize = str(video_path.stat().st_size)
-        headers = {
-            'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
-            'Accept-Ranges': 'bytes'
-        }
-        return Response(data, status_code=206, headers=headers, media_type="video/mp4")
+# @app.get("/video")
+# async def video_endpoint(range: str = Header(None)):
+#     start, end = range.replace("bytes=", "").split("-")
+#     start = int(start)
+#     end = int(end) if end else start + CHUNK_SIZE
+#     with open(video_path, "rb") as video:
+#         video.seek(start)
+#         data = video.read(end - start)
+#         filesize = str(video_path.stat().st_size)
+#         headers = {
+#             'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
+#             'Accept-Ranges': 'bytes'
+#         }
+#         return Response(data, status_code=206, headers=headers, media_type="video/mp4")
